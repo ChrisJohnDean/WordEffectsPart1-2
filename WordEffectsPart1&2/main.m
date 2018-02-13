8ba0855824b5d0c07fab6cab4079dbe4f0f3eb93
Chris Dean <chrisdean@Chris-Deans-MacBook-Pro.local>
2018-02-12 20:43:38 -0800

    Initial Commit

---
+++ WordEffectsPart1&2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WordEffectsPart1&2
+//
+//  Created by Chris Dean on 2018-02-12.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
